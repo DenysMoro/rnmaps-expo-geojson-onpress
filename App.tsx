d11d94fa50662395d3ae416e298129ff7171bb0b
Denys Morozov <denys.morozov@taranis.ag>
2021-06-16 13:58:38 +0300

    Initial maps

--- App.tsx
+++ App.tsx
@@ -1,15 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import RNM, { Geojson, MapEvent, Marker } from 'react-native-maps'
+
+const newGeoJson = require('./field_790470_1.json')
+const initialCoords = {
+  latitude: 33.61099089454942,longitude: -90.79735743461134
+}
 
 export default function App() {
+  const mapRef = useRef<RNM>(null)
+
+  useEffect(() => {
+    mapRef.current?.setCamera({ center: initialCoords });
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RNM
+        ref={mapRef}
+        provider="google"
+        showsCompass={false}
+        showsScale={false}
+        showsBuildings={false}
+        showsTraffic={false}
+        showsIndoors={false}
+        style={styles.map}
+        initialRegion={{
+          ...initialCoords,
+          latitudeDelta: 0.007,
+          longitudeDelta: 0.007
+        }}
+      >
+        <Geojson
+          geojson={newGeoJson}
+          fillColor="transparent"
+          strokeColor="#27c7be"
+          strokeWidth={2}
+          onPress={(test) => console.log(test)}
+        />
+      </RNM>
     </View>
   );
 }
+
+const {
+  width: dimensionsWindowWidth,
+  height: dimensionsWindowHeight,
+} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    height: dimensionsWindowHeight,
+    width: dimensionsWindowWidth,
+  }
 });
